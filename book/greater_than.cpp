3768fbfe2b825cd37075ccd18be02cc46a2b29f3
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-09-15 13:08:17 +0530

    clean things up

--- book/greater_than.cpp
+++
@@ -1,8 +0,0 @@
-    if (a > b)                                                          // 1 
-    {                                                                   // 2 
-        // CODE BLOCK IF TRUE                                           // 3 
-    }                                                                   // 4 
-    else                                                                // 5 
-    {                                                                   // 6 
-        // CODE BLOCK IF FALSE                                          // 7 
-    }                                                                   // 8 
